544b7595d3f150fbd23ad9f72825621ab26f6798
Büşra Ceylan <68326893+ceylanbusra@users.noreply.github.com>
2022-01-01 15:44:29 +0300

    read.Me created

---
+++ quiz-app/app/data/QuizData.js
@@ -0,0 +1,29 @@
+export default data = [
+    {
+        question: "Güneş'e en yakın gezegen hangisidir",
+        options: ["Dünya","Mars","Jüpiter","Merkür"],
+        correct_option: "Merkür"
+    },
+    {
+        question: "İlk bilgisyarın adı nedir ?",
+        options: ["Apple","Microsoft","Eniac"],
+        correct_option: "Eniac"
+    },
+    {
+        question: "Tarihteki ilk bilgisayar programcısı kimdir ?",
+        options: ["Ada Lovelace","Marrie curie","Thomson dalton","Nicola Tesla"],
+        correct_option: "Ada Lovelace"
+    },
+    {
+        question: "Yüz ölçümü en büyük il hangisidir ?",
+        options: ["Van","Şanlıurfa","Konya","Antalya"],
+        correct_option: "Konya"
+    },
+    {
+        question: "Nobel kimya ödülü alan Türk bilim adamı kimdir?",
+        options: ["Ilber Ortaylı","Orhan Pamuk","Gazi Yaşargil","Aziz Sancar"],
+        correct_option: "Aziz Sancar"
+    },
+   
+   
+]
